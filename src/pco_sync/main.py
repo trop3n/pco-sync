6cadc2e82e03ace6ddf3c458bfefc0d5448de80e
trop3n <jasondkimm@proton.me>
2025-03-25 21:28:49 -0500

    added logic for plan data retrieval

--- src/pco_sync/main.py
+++ src/pco_sync/main.py
@@ -26,3 +26,44 @@ class CalenderSync:
             'Authorization': f'Bearer {self.graph.token}',
             'Content-Type': 'application/json'
         }
+    def _get_microsoft_token(self):
+        authority = f'https://login.microsoftonline.com/{MICROSOFT_TENANT_ID}'
+        app = ConfidentialClientApplication(
+            client_id=MICROSOFT_CLIENT_ID,
+            client_credential=MICROSOFT_CLIENT_SECRET,
+            authority=authority
+        )
+        result = app.acquire_token_for_client(scopes=['https://graph.microsoft.com/.default'])
+        return result['access_token']
+    
+    def _get_pco_operator_events(self):
+        url = 'https://api.planningcenteronline.com/services/v2/service_types/.../plans'
+        params = {
+            'include': 'team_members',
+            'where[team_name]': 'Operator'
+            'per_page': 100
+        }
+
+        response = requests.get(url, auth=self.pco_auth, params=params)
+        events = []
+
+        for plan in response.json().get('data', []):
+            for member in plan.get('relationships', {}).get('team_members', {}).get('data', []):
+                event = {
+                    'subject': f"Operator Shift: {member.get('attributes', {}).get('name')}",
+                    'start': {
+                        'dateTime': plan['attributes']['starts_at'],
+                        'timeZone': 'America/Chicago'
+                    },
+                    'end': {
+                        'dateTime': plan['attributes']['ends_at'],
+                        'timeZone': 'America/Chicago'
+                    },
+                    'body': {
+                        'content': f"Service: {plan['attributes']['title']}\nPerson: {member['attributes']['name']}"
+                    }
+                }
+                events.append(event)
+            return events
+        
+        
