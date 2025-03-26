15bab25f3fec46d458565c07cd8a54cfca7161d6
trop3n <jasondkimm@proton.me>
2025-03-25 23:16:00 -0500

    more work

--- src/pco_sync/sync_script.py
+++ src/pco_sync/sync_script.py
@@ -38,3 +38,46 @@ class CalenderSync:
             authority=authority
         )
         return app.acquire_token_for_client(scopes=['https://graph.microsoft.com/.default'])['access_token']
+    
+    def _get_pco_events(self):
+        # Retrieve operator events from Planning Center Online
+        url = 'https://api.planningcenteronline.com/services/v2/service_types/.../plans'
+        params = {
+            'include': 'team_members',
+            'where[team_name]': 'Operator',
+            'per_page': 100
+        }
+
+        events = []
+        response = requests.get(url, auth=self.pco_auth, params=params)
+
+        for plan in response.json().get('data', []):
+            for member in plan.get('relationships', {}).get('team_members', {}).get('data', []):
+                events.append({
+                    'subject': f"Operator Shift: {member.get('attributes', {}).get('name')}",
+                    'start': {
+                        'dateTime': plan['attributes']['starts_at'],
+                        'timeZone': 'America/Chicago'
+                    },
+                    'end': {
+                        'dateTime': plan['attributes']['ends_at'],
+                        'timeZone': 'America/Chicago'
+                    },
+                    'body': {
+                        'content': f"Service {plan['attributes']['title']}\nPerson: {member['attributes']['name']}"
+                    },
+                    'singleValueExtendedProperties': [{
+                        'id': 'String {66f5a359-4659-4830-9070-000000000000} Name PCO_ID',
+                        'value': f"{plan['id']}|{member['id']}"
+                    }]
+                })
+            return events
+        
+        def _get_existing_outlook_events(self):
+            # retrieve existing outlook events with PCO identifiers
+            events = {}
+            url = f'https://graph.microsoft.com/v1.0/users/{self.calender_id}/events'
+            params = {
+                '$select': 'id',
+                '$expand': 'singleValueExtendedProperties($filter=id eq \'String {66f5a359-4659-4830-9070-000000000000} Name PCO_ID\')'
+            }
